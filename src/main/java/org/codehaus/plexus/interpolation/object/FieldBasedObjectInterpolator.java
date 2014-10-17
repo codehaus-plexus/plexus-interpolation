b3203ca6f3a0ad4d0a6e13fe362053e3ff6619e9
Kristian Rosenvold <krosenvold@apache.org>
2014-10-17 21:17:46 +0200

    Changed synhronization target

--- src/main/java/org/codehaus/plexus/interpolation/object/FieldBasedObjectInterpolator.java
+++ src/main/java/org/codehaus/plexus/interpolation/object/FieldBasedObjectInterpolator.java
@@ -260,7 +260,7 @@ public class FieldBasedObjectInterpolator
                     if ( isQualifiedForInterpolation( field, type ) )
                     {
                         boolean isAccessible = field.isAccessible();
-                        synchronized ( type )
+                        synchronized ( cls )
                         {
                             field.setAccessible( true );
                             try
