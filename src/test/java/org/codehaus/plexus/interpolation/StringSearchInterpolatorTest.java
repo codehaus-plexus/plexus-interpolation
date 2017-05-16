acbce69504291995022e8caa55004c1ec2825852
Gabriel Belingueres <belingueres@gmail.com>
2017-05-16 15:09:06 -0300

    Reseted branch commit

--- src/test/java/org/codehaus/plexus/interpolation/StringSearchInterpolatorTest.java
+++ src/test/java/org/codehaus/plexus/interpolation/StringSearchInterpolatorTest.java
@@ -17,17 +17,27 @@ package org.codehaus.plexus.interpolation;
  */
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.codehaus.plexus.interpolation.os.OperatingSystemUtils;
+import org.junit.Before;
+
 import junit.framework.TestCase;
 
 public class StringSearchInterpolatorTest
     extends TestCase
 {
+
+    @Before
+    public void setUp()
+    {
+        EnvarBasedValueSource.resetStatics();
+    }
 
     public void testLongDelimitersInContext()
         throws InterpolationException
@@ -147,14 +157,15 @@ public class StringSearchInterpolatorTest
         }
     }
 
-    public void testShouldResolveByMy_getVar_Method()
+    public void testShouldResolveByUsingObject_List_Map()
         throws InterpolationException
     {
         StringSearchInterpolator rbi = new StringSearchInterpolator();
         rbi.addValueSource( new ObjectBasedValueSource( this ) );
-        String result = rbi.interpolate( "this is a ${var}" );
+        String result =
+            rbi.interpolate( "this is a ${var} ${list[1].name} ${anArray[2].name} ${map(Key with spaces).name}" );
 
-        assertEquals( "this is a testVar", result );
+        assertEquals( "this is a testVar testIndexedWithList testIndexedWithArray testMap", result );
     }
 
     public void testShouldResolveByContextValue()
@@ -175,14 +186,24 @@ public class StringSearchInterpolatorTest
     public void testShouldResolveByEnvar()
         throws IOException, InterpolationException
     {
+        OperatingSystemUtils.setEnvVarSource( new OperatingSystemUtils.EnvVarSource()
+        {
+            public Map<String, String> getEnvMap()
+            {
+                HashMap<String,String> map = new HashMap<String,String>();
+                map.put( "SOME_ENV", "variable" );
+                map.put( "OTHER_ENV", "other variable" );
+                return map;
+            }
+        } );
+
         StringSearchInterpolator rbi = new StringSearchInterpolator();
 
         rbi.addValueSource( new EnvarBasedValueSource( false ) );
 
-        String result = rbi.interpolate( "this is a ${env.HOME} ${env.PATH}" );
+        String result = rbi.interpolate( "this is a ${env.SOME_ENV} ${env.OTHER_ENV}" );
 
-        assertFalse( "this is a ${HOME} ${PATH}".equals( result ) );
-        assertFalse( "this is a ${env.HOME} ${env.PATH}".equals( result ) );
+        assertEquals( "this is a variable other variable", result );
     }
 
     public void testUsePostProcessor_DoesNotChangeValue()
@@ -427,6 +448,46 @@ public class StringSearchInterpolatorTest
     public String getVar()
     {
         return "testVar";
+    }
+
+    public Person[] getAnArray()
+    {
+        Person[] array = new Person[3];
+        array[0] = new Person( "Gabriel" );
+        array[1] = new Person( "Daniela" );
+        array[2] = new Person( "testIndexedWithArray" );
+        return array;
+    }
+
+    public List<Person> getList()
+    {
+        List<Person> list = new ArrayList<Person>();
+        list.add( new Person( "Gabriel" ) );
+        list.add( new Person( "testIndexedWithList" ) );
+        list.add( new Person( "Daniela" ) );
+        return list;
+    }
+
+    public Map<String, Person> getMap()
+    {
+        Map<String, Person> map = new HashMap<String, StringSearchInterpolatorTest.Person>();
+        map.put( "Key with spaces", new Person( "testMap" ) );
+        return map;
+    }
+
+    public static class Person
+    {
+        private String name;
+
+        public Person( String name )
+        {
+            this.name = name;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
     }
 
 }
