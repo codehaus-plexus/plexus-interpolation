298285e7e974e5d39272e425bc4469cc3e4d306a
Sylwester Lachiewicz <slachiewicz@apache.org>
2025-11-03 13:54:21 +0100

    Remove unnecessary exports from module-info.java
    Removed multiple exports from the module definition.

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,9 +1,3 @@
 module org.codehaus.plexus.interpolation {
     exports org.codehaus.plexus.interpolation;
-    exports org.codehaus.plexus.interpolation.fixed;
-    exports org.codehaus.plexus.interpolation.multi;
-    exports org.codehaus.plexus.interpolation.object;
-    exports org.codehaus.plexus.interpolation.os;
-    exports org.codehaus.plexus.interpolation.reflection;
-    exports org.codehaus.plexus.interpolation.util;
 }
